087d13ab500cd39f45a018aa3ec56b546524fb84
nicolo_canevari <nico.canevari@outlook.it>
2025-02-04 15:03:10 +0100

    [FEAT] JS: prima parte su index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// importo il pacchetto express
+const express = require('express');
+// creo l'applicazione Express per configurare il server e le rotte
+const app = express();
+// porta su cui il server ascolterà le richieste HTTP
+const port =3000;
